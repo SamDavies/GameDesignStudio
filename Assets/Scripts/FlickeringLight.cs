113b995da79b5e5b0bb3f12eb2d9986e0448851f
Sam Davies <samdavies@live.co.uk>
2016-01-26 15:17:33 +0000

    Add flickering light script

---
+++ Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+	public Light light;
+	public float minIntensity;
+	public float maxIntensity;
+	public float maxChangePerUpdate;
+
+	// record the starting intensity
+	private float startIntensity;
+
+	// Use this for initialization
+	void Start () { 
+		startIntensity = light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// set a new random intensity in as a random number between the min and max intensity
+		float preIntensity = light.intensity;
+		float newIntensity = Random.Range(minIntensity, maxIntensity);
+
+		// limit the change in light intensity
+		newIntensity = Mathf.Clamp(newIntensity, preIntensity-maxChangePerUpdate, preIntensity+maxChangePerUpdate);
+		light.intensity = newIntensity;
+	}
+}
