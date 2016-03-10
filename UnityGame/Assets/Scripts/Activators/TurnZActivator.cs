3112bfe91c565b450ceaa9180c8ea4c9ae48d70b
Sam Davies <samdavies@live.co.uk>
2016-03-10 20:48:47 +0000

    Add shit

---
+++ UnityGame/Assets/Scripts/Activators/TurnZActivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnZActivator : AbstractTurnActivator {
+
+	public float showZ;
+	public float hideZ;
+
+	// Use this for initialization
+	void Start () {
+		showRot = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, showZ);
+		hideRot = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, hideZ);
+		this.setUp();
+	}
+}
